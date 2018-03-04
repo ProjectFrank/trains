f1a8c4e58e79fad3db77fb3c420e946c7ad39958
Frank Wang <frank.wangtao@gmail.com>
2018-03-04 11:59:53 -0500

    parse input into adjacency list

--- src/trains/core.clj
+++ src/trains/core.clj
@@ -1,5 +1,24 @@
 (ns trains.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.string :as string]))
+
+(defn edge [str]
+  (let [[tail head length] (string/split str #"")]
+    {:head head
+     :tail tail
+     :length (Integer/parseInt length)}))
+
+(defn adjacency-list [edges]
+  (->> edges
+       (group-by :tail)
+       (map (fn [[k v]]
+                [k (->> v
+                        (map (juxt :head identity))
+                        (into {}))]))
+       (into {})))
+
+(defn parse-input [input]
+  (adjacency-list (map edge (string/split input #",\s?"))))
 
 (defn -main
   "I don't do a whole lot ... yet."
--- test/trains/core_test.clj
+++ test/trains/core_test.clj
@@ -1,7 +1,24 @@
 (ns trains.core-test
   (:require [clojure.test :refer :all]
-            [trains.core :refer :all]))
+            [trains.core :as trains]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest edge-test
+  (is (= {:head "B", :length 5, :tail "A"}
+         (trains/edge "AB5"))))
+
+(deftest parse-input-test
+  (is (= {"A"
+          {"B" {:head "B", :length 5, :tail "A"}
+           "D" {:head "D", :length 5, :tail "A"}
+           "E" {:head "E", :length 7, :tail "A"}}
+          "B"
+          {"C" {:head "C", :length 4, :tail "B"}}
+          "C"
+          {"D" {:head "D", :length 8, :tail "C"}
+           "E" {:head "E", :length 2, :tail "C"}}
+          "D"
+          {"C" {:head "C", :length 8, :tail "D"}
+           "E" {:head "E", :length 6, :tail "D"}}
+          "E"
+          {"B" {:head "B", :length 3, :tail "E"}}}
+         (trains/parse-input "AB5, BC4, CD8, DC8, DE6, AD5, CE2, EB3, AE7"))))
