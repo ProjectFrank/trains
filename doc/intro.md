6abbf3457cdc50a110ed4e8f0abd73215694d594
Frank Wang <frank.wangtao@gmail.com>
2018-03-04 11:00:44 -0500

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to trains
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/trains/core_test.clj
@@ -0,0 +1,7 @@
+(ns trains.core-test
+  (:require [clojure.test :refer :all]
+            [trains.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
