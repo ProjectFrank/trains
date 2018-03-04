995f9e5f2508d7bd67e30e57bff69895bda9ab8d
Frank Wang <frank.wangtao@gmail.com>
2018-03-04 17:07:33 -0500

    print solutions in -main

--- src/trains/core.clj
+++ src/trains/core.clj
@@ -1,6 +1,16 @@
 (ns trains.core
   (:gen-class)
-  (:require [clojure.string :as string]))
+  (:require [clojure.string :as string]
+            [trains.ex1 :as ex1]
+            [trains.ex2 :as ex2]
+            [trains.ex3 :as ex3]
+            [trains.ex4 :as ex4]
+            [trains.ex5 :as ex5]
+            [trains.ex6 :as ex6]
+            [trains.ex7 :as ex7]
+            [trains.ex8 :as ex8]
+            [trains.ex9 :as ex9]
+            [trains.ex10 :as ex10]))
 
 (defn edge [str]
   (let [[tail head length] (string/split str #"")]
@@ -17,10 +27,35 @@
                         (into {}))]))
        (into {})))
 
-(defn parse-input [input]
-  (adjacency-list (map edge (string/split input #",\s?"))))
+(defn parse-input [standardized-input]
+  (adjacency-list (map edge (string/split standardized-input #",\s*"))))
+
+(defn standardize-input [input]
+  (string/join " " input))
+
+(defn report [input]
+  (let [graph (-> input
+                  standardize-input
+                  parse-input)
+        solutions [(ex1/solution graph)
+                   (ex2/solution graph)
+                   (ex3/solution graph)
+                   (ex4/solution graph)
+                   (ex5/solution graph)
+                   (ex6/solution graph)
+                   (ex7/solution graph)
+                   (ex8/solution graph)
+                   (ex9/solution graph)
+                   (ex10/solution graph)]]
+    (->> solutions
+         (map (fn [exercise-number solution]
+                (format "Output #%s: %s"
+                        exercise-number
+                        solution))
+              (rest (range)))
+         (string/join "\n"))))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println (report args)))
--- test/trains/core_test.clj
+++ test/trains/core_test.clj
@@ -1,10 +1,10 @@
 (ns trains.core-test
   (:require [clojure.test :refer :all]
-            [trains.core :as trains]))
+            [trains.core :as core]))
 
 (deftest edge-test
   (is (= {:head "B", :length 5, :tail "A"}
-         (trains/edge "AB5"))))
+         (core/edge "AB5"))))
 
 (deftest parse-input-test
   (is (= {"A"
@@ -21,4 +21,10 @@
            "E" {:head "E", :length 6, :tail "D"}}
           "E"
           {"B" {:head "B", :length 3, :tail "E"}}}
-         (trains/parse-input "AB5, BC4, CD8, DC8, DE6, AD5, CE2, EB3, AE7"))))
+         (core/parse-input "AB5, BC4, CD8, DC8, DE6, AD5, CE2, EB3, AE7"))))
+
+(deftest report-test
+  (is (= "Output #1: 9\nOutput #2: 5\nOutput #3: 13\nOutput #4: 22\nOutput #5: NO SUCH ROUTE\nOutput #6: 2\nOutput #7: 3\nOutput #8: 9\nOutput #9: 9\nOutput #10: 7"
+         (core/report (list "AB5, BC4, CD8, DC8, DE6, AD5, CE2, EB3, AE7"))))
+  (is (= "Output #1: 9\nOutput #2: 5\nOutput #3: 13\nOutput #4: 22\nOutput #5: NO SUCH ROUTE\nOutput #6: 2\nOutput #7: 3\nOutput #8: 9\nOutput #9: 9\nOutput #10: 7"
+         (core/report (list "AB5," "BC4," "CD8," "DC8," "DE6," "AD5," "CE2," "EB3," "AE7")))))
